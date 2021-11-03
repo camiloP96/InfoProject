1d505cca5f5eda84bd1a0abb2c2ff3d6ec40b4fe
camiloP96 <cristiancami1@hotmail.com>
2021-11-03 14:08:13 -0500

    infopage Project NextJs init

---
+++ pages/admin/usuarios/AddUser.tsx
@@ -0,0 +1,11 @@
+import type {NextPage} from 'next';
+
+const AddUser:NextPage = () => {
+    return (
+        <div>
+            Pagina de añadir usuarios
+        </div>
+    )
+}
+
+export default AddUser

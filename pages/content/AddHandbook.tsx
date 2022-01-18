1b13ff786eacd806a2f1f8ff8894a6fffeeb1a01
camiloP96 <cristiancami1@hotmail.com>
2022-01-18 00:17:44 -0500

    add page AddHandbook and AddViodeConference

--- pages/content/AddHandbook.tsx
+++ pages/content/AddHandbook.tsx
@@ -2,8 +2,46 @@ import React from 'react'
 
 const AddHandbook = () => {
     return (
-        <div>
-            hi handbook
+        <div className='text-center grid grid-cols-3 gap-4 items-center'>
+            <div className='col-span-3'>
+                <label htmlFor='Name'>Name</label>
+                <input  type='text' id='Name' name='Name' className=' bg-slate-700 focus:outline-none'></input>
+            </div>
+            
+            <div className='col-span-3'>
+                <label htmlFor='DescriptionH'>Description</label>
+                <input  type='text' id='DescriptionH' name='Description' className=' bg-slate-700 focus:outline-none'></input>
+            </div>
+
+            <div>
+                <label htmlFor='AuthorNameH'> Author</label>
+                <input  type='text' id='AuthorNameH' name='AuthorNameH' className=' bg-slate-700 focus:outline-none'></input>
+            </div>
+
+            <div>
+                <label htmlFor='YearH'>Year</label>
+                <input  type='number' id='YearH' name='YearH' className=' bg-slate-700 focus:outline-none'></input>
+            </div>
+
+            <div>
+                <label htmlFor='ISBN'>ISBN</label>
+                <input  type='text' id='ISBN' name='ISBN' className=' bg-slate-700 focus:outline-none'></input>
+            </div>
+
+            <div className='col-span-2'>
+                <label htmlFor='LinkH'>Link</label>
+                <input  type='url' id='LinkH' name='LinkH' className=' bg-slate-700 focus:outline-none'></input>
+            </div>
+
+            <div>
+                <label htmlFor='Domain'>Domain</label>
+                <input  type='text' id='Domain' name='Domain' className=' bg-slate-700 focus:outline-none'></input>
+            </div>
+
+            <button className='col-span-3  bg-slate-700 focus:outline-none'>
+                <h1>submitt</h1>
+            </button>
+
         </div>
     )
 }
--- pages/content/AddVideoc.tsx
+++ pages/content/AddVideoc.tsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 const AddVideoc = () => {
     return (
-        <div>
-            hi video
+        <div className='text-center grid grid-cols-2 gap-5'>
+            <div > 
+            <label htmlFor='Link'>Link</label>    
+            <input className=' bg-slate-700 focus:outline-none' type='url' id='Link' name='Link' required></input>
+            </div>
+           
+            <div className=''>
+                <label htmlFor='Topic'>Topic</label>
+                <input className=' bg-slate-700 focus:outline-none' type='text' id='Topic' name='Topic'></input>
+            </div>
+
+            <div className='col-span-2'> 
+                <label htmlFor='Description'>Description</label>
+                <input className=' bg-slate-700 focus:outline-none'  type='text' id='Description' name='Description' ></input>
+            </div>
+
+            <button className='col-span-2'>
+                
+                <h1>Submitt</h1>
+                
+            </button>
         </div>
     )
 }
